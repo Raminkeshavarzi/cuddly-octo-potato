9b50de5aa78f7b115074eefed5cc5552494d0139
Ramin Keshavarzi <raminkeshavarzi@organiplace.com>
2021-03-11 17:21:12 +0100

    added initial app

---
+++ Magic-Ball/Magic-Ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic-Ball
+//
+//  Created by Ramin on 11/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
