f55e0716786a326dfcc5dd52bfa805ba3572094f
Ramin Keshavarzi <raminkeshavarzi@organiplace.com>
2021-03-11 18:54:47 +0100

    Added the logics for changing UI

--- Magic-Ball/Magic-Ball/ViewController.swift
+++ Magic-Ball/Magic-Ball/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+//Interface builder
+    @IBOutlet weak var imgView: UIImageView!
+    
+//Global Variable
+    let answerImg = [ #imageLiteral(resourceName: "ball3"), #imageLiteral(resourceName: "ball2"), #imageLiteral(resourceName: "ball2"), #imageLiteral(resourceName: "ball3"), #imageLiteral(resourceName: "ball4") ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+//Function
+    
+    @IBAction func getTheAnswer(_ sender: Any) {
+        let randomAnswer = Int.random(in: 0 ... 4)
+        imgView.image = answerImg[randomAnswer]
+    }
+    
+    
 }
 
